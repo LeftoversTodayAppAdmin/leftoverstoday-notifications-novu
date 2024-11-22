91a4c193f2feced977807d21f3ff5474958478ab
Shrinidhi Shamasunder <operations@leftovers.today>
2024-11-22 18:54:58 +0000

    Feat - Updated config files

--- libs/embed/src/shared/resources.js
+++ libs/embed/src/shared/resources.js
@@ -5,21 +5,21 @@ if (process.env.ENVIRONMENT === 'production') {
     API_URL: '',
     IFRAME_URL: process.env.WIDGET_URL || 'https://widget.novu.co',
     WWW_URL: process.env.WIDGET_URL || 'https://widget.novu.co',
-    SENTRY_DSN: 'https://bcadda4b593d454797a1260e094f0c5d@o1161119.ingest.sentry.io/6252448',
+    SENTRY_DSN: 'https://01da3a478106f3dc4448ce2edfc2b4a7@o4508090488127488.ingest.us.sentry.io/4508204170608640',
   };
 } else if (process.env.ENVIRONMENT === 'dev') {
   resourcesConstants = {
     API_URL: '',
     IFRAME_URL: process.env.WIDGET_URL || 'https://dev.widget.novu.co',
     WWW_URL: process.env.WIDGET_URL || 'http://127.0.0.1:3500',
-    SENTRY_DSN: '',
+    SENTRY_DSN: 'https://01da3a478106f3dc4448ce2edfc2b4a7@o4508090488127488.ingest.us.sentry.io/4508204170608640',
   };
 } else {
   resourcesConstants = {
     API_URL: '',
     IFRAME_URL: process.env.WIDGET_URL || 'http://127.0.0.1:3500',
     WWW_URL: process.env.WIDGET_URL || 'http://127.0.0.1:3500',
-    SENTRY_DSN: '',
+    SENTRY_DSN: 'https://01da3a478106f3dc4448ce2edfc2b4a7@o4508090488127488.ingest.us.sentry.io/4508204170608640',
   };
 }
 
